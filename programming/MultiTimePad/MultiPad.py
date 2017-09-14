f8673863e0a742576db66827bd05438d0fd7c51d
Christian Schaffner <c.schaffner@uva.nl>
2017-09-14 08:07:51 +0200

    adding programming stuff

---
+++ programming/MultiTimePad/MultiPad.py
@@ -0,0 +1,52 @@
+# Python 2 / 3
+# author: Christian Schaffner
+
+KEY_LENGTH = 31
+KEY = ['00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
+# of course, I did not use the all-0 key when generating the ciphertexts
+
+"""
+useful python commands:
+
+    ord(c) -> integer
+    Return the integer ordinal of a one-character string.
+    Example: ord('d') == 100
+
+    chr(i) -> character    
+    Return a string of one character with ordinal i; 0 <= i < 256.
+    Example: chr(100) == 'd'
+
+    int(x, base=16) -> int or long        
+    Converts hexadecimal string x to an integer
+    Example:  int('7b', 16) == 123
+
+    '{0:02x}'.format(integer)
+    returns the hexadecimal representation (at least two characters) of an integer
+    Example: '{0:02x}'.format(123)  ==  '7b'
+
+    '{0:08b}'.format(integer
+    returns the binary representation (at least 8 characters) of an integer
+    Example: '{0:08b}'.format(23) == '00010111'
+
+    x.__xor__(y) <==> x^y   
+    bitwise XOR of x and y
+    Example: 23 ^ 156 == 139 , because  00010111 XOR 10011100 == 10001011
+"""
+
+
+def main():
+    fout = open('ciphertexts.txt', 'w')
+    for line in  open('messages.txt', 'r'):
+        ciphertext = ''
+        pos = 0
+        for char in line.rstrip(): # ignore the \n character at the end of the line if it's there
+            ciphertext += '{0:02x}'.format((int(ord(char)) ^ int(KEY[pos], 16)))
+            pos += 1
+        fout.write('{}\n'.format(ciphertext))
+
+    fout.close()
+
+
+
+if __name__ == "__main__":
+    main()
